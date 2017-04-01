eee6852337d707ae45ce0be773d52ccbebcb6573
ghwa <chengjeehwa@gmail.com>
2017-04-01 10:56:18 +0800

    First Push
    First Push

---
+++ app/src/main/java/com/jeehwa/funfacts/ColorWheel.java
@@ -0,0 +1,35 @@
+package com.jeehwa.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by jeehwa on 31/03/2017.
+ */
+
+public class ColorWheel {
+
+    private String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    public String getColor() {
+        String color = "";
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        color = mColors[randomNumber];
+
+        return color;
+    }
+}
---
+++ app/src/main/java/com/jeehwa/funfacts/FunFactsActivity.java
@@ -0,0 +1,43 @@
+package com.jeehwa.funfacts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static java.lang.Integer.parseInt;
+
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String fact = mFactBook.getFact();
+                String color = mColorWheel.getColor();
+                mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(parseInt(color)); // find a way to convert # color code to int
+            }
+        };
+
+        mShowFactButton.setOnClickListener(listener);
+    }
+}
